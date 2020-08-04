4c5a38419ff08efb0079189149f3939e51591d33
AshutoshSundresh <62063875+AshutoshSundresh@users.noreply.github.com>
2020-08-04 08:18:03 +0530

    Fix bootloop on system only devices (3/3)

--- lawnchair.mk
+++ lawnchair.mk
@@ -4,6 +4,10 @@ PRODUCT_COPY_FILES += \
     vendor/lawnchair/etc/permissions/privapp-permissions-ch.deletescape.lawnchair.ci.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-ch.deletescape.lawnchair.ci.xml \
     vendor/lawnchair/etc/sysconfig/lawnchair-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lawnchair-whitelist.xml
 
+PRODUCT_COPY_FILES += \
+    vendor/lawnchair/etc/permissions/privapp-permissions-ch.deletescape.lawnchair.ci.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-ch.deletescape.lawnchair.ci.xml \
+    vendor/lawnchair/etc/sysconfig/lawnchair-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-whitelist.xml
+
 PRODUCT_PACKAGES += \
     QuickstepSwitcherOverlay \
     Lawnchair \
