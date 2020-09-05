f86f9b78303690f2ee03ed68a7d3484f3e81aafa
Raaj52 <mail2raaj52@gmail.com>
2020-09-05 16:29:53 +0000

    Lawnchair: Bringing up for ZenX-OS!
    - Updated Lawnchair.apk to 2.1-2627-ci-q-merge
    - Added Lawnfeed.apk
    - Override default Pixel Launcher if opted
    - Nuked README.md
    
    Credits - @deletescape and the team for the beautiful yet stable launcher, @arter97 for the Lawnchair AOSP based build system and @skittles9823 for  Quickstep
    
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

--- priv-app/Lawnchair/Android.mk
+++ priv-app/Lawnchair/Android.mk
@@ -1,12 +1,23 @@
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := Lawnchair
-LOCAL_MODULE_STEM := Lawnchair.apk
-LOCAL_SRC_FILES := Lawnchair.apk
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_STEM := Lawnchair.apk
 LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app/Lawnchair
-LOCAL_OVERRIDES_PACKAGES := ParanoidQuickStep Launcher3QuickStep PixelLauncher TrebuchetQuickStep
+LOCAL_SRC_FILES := Lawnchair.apk
+LOCAL_MODULE_CLASS := ETC
+LOCAL_OVERRIDES_PACKAGES := ParanoidQuickStep Launcher3QuickStep PixelLauncher NexusLauncherRelease TrebuchetQuickStep
+include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := Lawnfeed
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/$(TARGET_COPY_OUT_PRODUCT)/app
+LOCAL_SRC_FILES := Lawnfeed.apk
+LOCAL_DEX_PREOPT := false
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
